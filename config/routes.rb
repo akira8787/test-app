4362b046e37f11bccd0b30fa238164885ef1011c
akira8787 <daigocafe222@gmail.com>
2023-05-25 03:54:44 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :posts, only:[:index, :new, :create, :edit, :update, :destroy]
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
